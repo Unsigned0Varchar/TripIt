cef4a4532ff844c8afe257ed6f22cb0343486a45
Abhikraj Dutta Choudhury <123456789abhik@gmail.com>
2025-07-05 14:58:45 +0530

    Fix: add vercel.json for dynamic routing

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  
+plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist"
+  },
+  base: "/", // make sure base is set correctly for routing
+});
